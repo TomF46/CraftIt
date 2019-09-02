6a6d3f9e0a4c9b80bd810996bb8aa48447b738d1
TomF46 <t-d-f@live.co.uk>
2019-09-02 14:03:11 +0100

    Remove old front end from project and make pure api project

--- craftit-frontend/src/boot/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default async ({ Vue }) => {
-  var http = axios.create({
-    baseURL: 'https://localhost:5001/api/',
-    timeout: 5000
-  })
-  Vue.prototype.$axios = http
-}
