091cab3a4f2e79abba858200a6988dbd05ae1869
TomF46 <t-d-f@live.co.uk>
2019-09-04 13:39:16 +0100

    Shorten jwt token expirey

--- CraftIt.Api/Controllers/AuthenticationController.cs
+++ CraftIt.Api/Controllers/AuthenticationController.cs
@@ -51,7 +51,7 @@ namespace WebApi.Controllers
                 {
                     new Claim(ClaimTypes.Name, user.Id.ToString())
                 }),
-                Expires = DateTime.UtcNow.AddDays(7),
+                Expires = DateTime.UtcNow.AddDays(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
